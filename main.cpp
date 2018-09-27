b2561bb65c75bbfe3ca3e6e83065fe0bd5efb614
aliakbar1 <aakbar2@mail.csuchico.edu>
2018-09-26 17:56:06 -0700

    fixed and dubbeged

--- main.cpp
+++ main.cpp
@@ -8,12 +8,53 @@ int main()
   int numItems = 0;
   char input;
 
+   string newitem;
+   do
+{ 
+        
+    
+   
+    
+
   cout<<"\n==GROCERY LIST MANAGER==";
+
   cout<<"\nEnter your choice: ";
+  
   cout<<"\n (A)dd an item";
+  
+
   cout<<"\n (Q)uit";
+  
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
+     if (input=='A'||input=='a')
+    {
+            cout<<"What is the item?";
+            cin>>newitem;
+         
+            
+      
+    if(numItems>=5)
+    {
+            cout<<"You'll need a bigger list!";
+    }
+    else {
 
+    
+            list[numItems]=newitem;
+            numItems++;
+    }        
+             
+    
+    
+    }
+}               
+
+
+
+
+
+   while(input!='q'&&input!='Q');
+            
   return 0;
 }
